bc1b5d84854eee6cf6a8fd4c3e2d61df99180fe2
Kate Lait <katelait@icloud.com>
2018-11-13 14:56:50 +0000

    adding images

--- ticker/main/charts.py
+++ ticker/main/charts.py
@@ -1,6 +1,7 @@
 from jchart import Chart
 from jchart.config import Axes, DataSet, rgba
 from .models import Price
+import time
 
 class ScatterLineChart(Chart):
     chart_type = 'line'
@@ -9,25 +10,44 @@ class ScatterLineChart(Chart):
     }
 
     def get_datasets(self, **kwargs):
-        time_set = ['2018-11-14T21:00:00', '2018-11-14T21:20:00', '2018-11-14T21:40:00', '2018-11-14T22:00:00', '2018-11-14T22:20:00', '2018-11-14T22:40:00', '2018-11-14T23:00:00']
+        time_set = ['2018-11-14 21:00:00', '2018-11-14 21:20:00', '2018-11-14 21:40:00', '2018-11-14 22:00:00', '2018-11-14 22:20:00', '2018-11-14 22:40:00', '2018-11-14 23:00:00']
         interval = 5
         fosters = []
         g_and_t = []
         captain_morgan = []
         pampero = []
         roe_and_co = []
-        zaconey = []
+        jaegerbomb = []
         vodka_redbull = []
-        prices = Price.objects.all()
+        prices = [PriceStatic(5.80, 9.20, 8.40, 15.80, 9.20, 16.00, 19.80, 0), PriceStatic(2.50, 9.00, 8.40, 10.00, 6.00, 10.00, 19.80, 1),
+        PriceStatic(5.80, 6.00, 8.40, 10.00, 6.00, 16.00, 19.80, 2), PriceStatic(2.50, 6.00, 8.40, 15.80, 9.20, 16.00, 10.00, 3),
+        PriceStatic(5.80, 9.20, 5.00, 10.00, 9.20, 10.00, 10.00, 4), PriceStatic(2.50, 9.20, 5.00, 10.00, 6.00, 16.00, 19.80, 5),
+        PriceStatic(2.50, 6.00, 8.40, 15.80, 6.00, 10.00, 19.80, 6),PriceStatic(5.80, 9.20, 8.40, 15.80, 9.20, 10.00, 10.00, 7),
+        PriceStatic(5.8, 6.00, 5.00, 10.00, 9.20, 16.00, 10.00, 8),PriceStatic(2.50, 9.20, 8.40, 10.00, 6.00, 16.00, 10.00, 9)]
+
+        hour= 21
+        minute=00
+        interval = 0
+        max_interval = 9
+        time_set2 = [{'hour': 21, 'minute': 00}, {'hour': 21, 'minute': 20}, {'hour': 21, 'minute': 40}, {'hour': 22, 'minute': 00}, {'hour': 22, 'minute': 20}, {'hour': 22, 'minute': 40}, {'hour': 23, 'minute': 00}]
+        localtime = time.localtime(time.time())
+        local_hour = localtime.tm_hour
+        local_minute = localtime.tm_min
+
+        current_time = time_set2[interval]
+        if interval!=max_interval:
+            next_time = time_set2[interval+1]
+            if local_hour==next_time['hour'] and local_minute==next_time['minute']:
+                interval = interval +1
         for price in prices:
-            if price.interval_index < interval:
-                fosters.append({'y' : price.foster, 'x': time_set[int(price.interval_index-1)] })
-                g_and_t.append({'y' : price.g_t, 'x': time_set[int(price.interval_index-1)]})
-                captain_morgan.append({'y' : price.captain_morgan, 'x': time_set[int(price.interval_index-1)]})
-                pampero.append({'y' : price.pampero, 'x': time_set[int(price.interval_index-1)]})
-                roe_and_co.append({'y' : price.roe_and_co, 'x': time_set[int(price.interval_index-1)]})
-                zaconey.append({'y' : price.zaconey, 'x': time_set[int(price.interval_index-1)]})
-                vodka_redbull.append({'y' : price.vodka_redbull, 'x': time_set[int(price.interval_index-1)]})
+            if price.interval_index==interval:
+                fosters.append({'y' : price.foster, 'x': time_set[int(price.interval_index)] })
+                g_and_t.append({'y' : price.g_t, 'x': time_set[int(price.interval_index)]})
+                captain_morgan.append({'y' : price.captain_morgan, 'x': time_set[int(price.interval_index)]})
+                pampero.append({'y' : price.pampero, 'x': time_set[int(price.interval_index)]})
+                roe_and_co.append({'y' : price.roe_and_co, 'x': time_set[int(price.interval_index)]})
+                jaegerbomb.append({'y' : price.jaegerbomb, 'x': time_set[int(price.interval_index)]})
+                vodka_redbull.append({'y' : price.vodka_redbull, 'x': time_set[int(price.interval_index)]})
 
 
         return [
@@ -53,12 +73,23 @@ class ScatterLineChart(Chart):
                     borderColor='yellow',
                     data=roe_and_co),
             DataSet(type='line',
-                    label='Zaconey & Splash',
+                    label='Double Jaeger-Bomb',
                     borderColor='purple',
-                    data=zaconey),
+                    data=jaegerbomb),
             DataSet(type='line',
                     label='Double Vodka Redbull',
                     borderColor='pink',
                     data=vodka_redbull)
 
         ]
+
+class PriceStatic:
+    def __init__(self, fosters, g_and_t, captain_morgan, pampero, roe_and_co, jaegerbomb, vodka_redbull, interval_index):
+        self.foster = fosters
+        self.g_t = g_and_t
+        self.captain_morgan = captain_morgan
+        self.pampero = pampero
+        self.roe_and_co = roe_and_co
+        self.jaegerbomb = jaegerbomb
+        self.vodka_redbull = vodka_redbull
+        self.interval_index = interval_index
--- ticker/main/views.py
+++ ticker/main/views.py
@@ -2,6 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from jchart import Chart
 from .models import Price
+from datetime import datetime
+import time
+from . import charts
+from . import views
+from jchart.views import ChartView
 
 def index(request):
-    return render(request, 'main/index.html')
+    hour= 21
+    minute=00
+    interval = 0
+    time_set = [{'hour': 21, 'minute': 00}, {'hour': 21, 'minute': 20}, {'hour': 21, 'minute': 40}, {'hour': 22, 'minute': 00}, {'hour': 22, 'minute': 20}, {'hour': 22, 'minute': 40}, {'hour': 23, 'minute': 00}]
+    localtime = time.localtime(time.time())
+    current_hour = localtime.tm_hour
+    current_minute = localtime.tm_min
+    if (current_hour> hour or current_minute>minute):
+        if (interval+1< 10):
+            next_time = time_set[interval+1]
+            if next_time['hour']==localtime.tm_hour and localtime.tm_min==next_time['minute']:
+                interval = interval+1
+
+    ChartView.from_chart(charts.ScatterLineChart())
+    return render(request, 'main/index.html', {'interval': interval})
+
+    
